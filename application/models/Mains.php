f5acdb7aa43498de2b435792e60fda20be450b4c
Ted Bataller <ted.jerbybataller@gmail.com>
2023-02-07 17:44:26 +0800

    Start shopping spree activity

---
+++ application/models/Mains.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mains extends CI_Model
+{
+    public function index()
+    {
+        $now = new DateTime('now', new DateTimeZone('Asia/Manila'));
+        $today = $now->format('F d, Y');
+        $tomorrow = new DateTime('tomorrow', new DateTimeZone('Asia/Manila'));
+        $difference = $now->diff($tomorrow);
+        $seconds = $difference->format('%s');
+        $hours = $difference->format('%h') * 60 * 60;
+        $remain = $hours + $seconds;
+        $view_data['now'] = $today;
+        $view_data['remain'] = $remain;
+        $this->load->view('Countdown/index', $view_data);
+    }
+}
+
+?>
